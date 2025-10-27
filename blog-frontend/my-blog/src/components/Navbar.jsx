bad8b278fd932edf25c776d763b6a2f9bf19e124
LamLam <ailamtran1812@gmail.com>
2025-10-27 15:14:59 +0100

    navbar

--- blog-frontend/my-blog/src/components/Navbar.jsx
+++ blog-frontend/my-blog/src/components/Navbar.jsx
@@ -2,27 +2,28 @@ import {Link} from "react-router-dom";
 
 export default  function Navbar(){
     return (
-        <nav className="bg-#0a0a1a border-b-2 border-red-500 shadow-0_0_10px_#ff003c">
-            <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between box-glow">
-                Logo
-                <div className="flex items-center gap-2">
-                    <span className="text-glow">FLUFFY</span>
-                    <span className="text-glow">1.0</span>
+        <nav>
+            <div>
+                <div>
+                    <span className="logo-top">Logo</span>
                 </div>
-                <ol>
-                    <span className="m-10 p-10 grid grid-rows-4 gap-4 text-glow">
-                        <Link to="/" className="text-2xl font-bold text-blue-600 p-20">My Blog</Link>
-                    </span>
-                    <span className="m-10 p-10 grid grid-rows-4 gap-4 text-glow">
-                        <Link to="/" className="bg-blue-100 p-20">Home</Link>
-                    </span>
-                    <span className="m-10 p-10 grid grid-rows-4 gap-4 text-glow">
-                        <Link to="/create" className="bg-blue-100 p-20">New Post</Link>
-                    </span>
-                    <span className="m-10 p-10 grid grid-rows-4 gap-4 text-glow">
-                        <Link className="bg-blue-300 p-20" to="/about">About me</Link>
-                    </span>
-                </ol>
+                <ul className="navbar box-glow">
+                    <li className="a">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="a">
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li className="a">
+                        <Link to="/blogs">Blogs</Link>
+                    </li>
+                    <li className="a">
+                        <Link to="/memories">Memories lane</Link>
+                    </li>
+                    <li className="a">
+                        <Link to="/about">About me</Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     )
--- blog-frontend/my-blog/src/pages/Home.jsx
+++ blog-frontend/my-blog/src/pages/Home.jsx
@@ -2,25 +2,10 @@ import React from "react";
 import background from "../assets/background.jpg"
 
 export default function Home (){
-    const backgroundimg = {
-        height: "100vh",
-        width: "150vh",
-        backgroundImage : `url(${background})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        color: "white"
-    };
     return (
-        <div>
-            <h1>
-                Welcome to Fluffy's Blog
-            </h1>
-            <p>Helllooooooooo</p>
-            <p style={backgroundimg}></p>
-        </div>
+        <body className="body-img">
+            <div>
+            </div>
+        </body>
     );
 }
